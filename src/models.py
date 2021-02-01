88e3c59b933c2b6672e80d27936d9e959229ae24
keerthisaran <keerthisaran@gmail.com>
2021-02-01 01:56:40 +0000

    add birectional model

--- src/models.py
+++ src/models.py
@@ -142,12 +142,94 @@ class Enc2DecWithAttention(torch.nn.Module):
 
 
 
+class Enc2DecWithAttentionBidir(torch.nn.Module):
+
+    def __init__(self,input_dim,encoder_decoder_hidden_dim,output_dim,attention_layers_dim=None):
+        super().__init__()
+        self.input_dim=input_dim
+        self.encoder_decoder_hidden_dim=encoder_decoder_hidden_dim
+        self.output_dim=output_dim
+
+        self.encoder=torch.nn.GRU(self.input_dim,self.encoder_decoder_hidden_dim,bidirectional=True)
+        self.encoder2hiddendim=torch.nn.Linear(self.encoder_decoder_hidden_dim*2,self.encoder_decoder_hidden_dim)
+
+
+        self.attention_layer=torch.nn.Linear(2*encoder_decoder_hidden_dim,1)
+        self.attention_softmax=torch.nn.Softmax(dim=-1)
+
+        self.decoder=torch.nn.GRU(self.output_dim+self.encoder_decoder_hidden_dim,encoder_decoder_hidden_dim)
+
+        self.output_layer=torch.nn.Linear(self.encoder_decoder_hidden_dim,self.output_dim)        
+        self.output_softmaxlog=torch.nn.LogSoftmax(dim=-1)
+
+        self.engine=PytorchEngine(self)
+
+
+    def forwardsample(self,X,max_len,ground_truth):
+        '''
+        X is a torch.tensor with shape [None,input_shape(English Vocabulary)] and dtype torch.float32
+        Y is a 1d tensors with shape [None,] and dtype torch.long
+
+        '''
+        
+        
+        enc_hiddens,_=self.encoder(X.unsqueeze(1))
+        enc_hiddens=self.encoder2hiddendim(enc_hiddens)
+        enc_hiddens=torch.nn.ReLU()(enc_hiddens)
+
+        last_enc_hidden=enc_hiddens[-1:]
+        prev_dec_hidden=last_enc_hidden
+        prev_dec_input=torch.zeros(1,1,self.output_dim)
+
+        enc_seq_len=enc_hiddens.shape[0]
+        outputs=[]
+        for i in range(max_len):
+            #enc_hiddens shape is (None,1,encoder_decoder_hidden)
+            attention_input=torch.cat([ prev_dec_hidden.repeat(enc_seq_len,1,1),
+                                        enc_hiddens],dim=-1)
+            attention_logits=self.attention_layer(attention_input.squeeze(1))
+            attention_logits=torch.nn.ReLU()(attention_logits)
+
+            attention_wts=self.attention_softmax(attention_logits.squeeze(-1))
+    
+            enc_input=torch.matmul(attention_wts,enc_hiddens.squeeze(1))
+            dec_input=torch.cat([enc_input,prev_dec_input.view(-1)])
+
+            _,prev_dec_hidden,=self.decoder(dec_input.view(1,1,-1),prev_dec_hidden)
+
+            output=self.output_layer(prev_dec_hidden.view(-1))
+            output=self.output_softmaxlog(output)
+            with torch.no_grad():
+                prev_dec_input=torch.zeros_like(prev_dec_input,dtype=torch.float32)
+                argmax_output=torch.argmax(output) if ground_truth is None else ground_truth[i]
+                prev_dec_input[0,0,argmax_output]=1.
+
+            outputs.append(output)
+        return torch.stack(outputs)
+    
+    def forward(self,X_batch,maxlens,ground_truth=None):
+        batch_pred=[]
+        for i,(X,maxlen) in enumerate(zip(X_batch,maxlens)):
+            ground_truth_i=ground_truth[i] if ground_truth else None
+            Y_hat=self.forwardsample(X,maxlen,ground_truth_i)
+            batch_pred.append(Y_hat)
+        
+        return batch_pred
+    
+    def fit(self,*args,**kwargs):
+        return self.engine.fit(*args,**kwargs)
+    
+    def compile(self,*args,**kwargs):
+        return self.engine.compile(*args,**kwargs)
+
+
+
 if __name__ == "__main__":
 
     X=[torch.zeros(7,24,dtype=torch.float32) for _ in range(10)]
     Y=[torch.zeros(6,dtype=torch.long) for _ in range(10)]
 
-    net=Enc2DecWithAttention(24,256,9)
+    net=Enc2DecWithAttentionBidir(24,256,9)
     maxlens=[len(w) for w in Y]
     print(net.forward(X,maxlens,Y))
     pass
